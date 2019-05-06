bb5787bd8f2aec86af32952ac0679c20e89a1972
velimd <velican.duzgunce@solirius.com>
2019-05-06 11:20:42 +0100

    Added controllers

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@ var myGamePiece;
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(140, 10, "red", 10, 120);
+    myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
 var myGameArea = {
@@ -13,6 +13,13 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0])
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        });
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = false;
+        });
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -22,6 +29,8 @@ var myGameArea = {
 function component(width, height, colour, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
     this.update = function() {
@@ -29,9 +38,41 @@ function component(width, height, colour, x, y) {
         ctx.fillStyle = colour;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
+    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
+    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
+    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();
     myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+
+function moveright() {
+    myGamePiece.speedX += 1;
+}
+
+function stopMove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
 }
